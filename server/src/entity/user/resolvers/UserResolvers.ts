d388037c9245584525108a9df02e13eb393f93a8
purpleshell <purpleshell@github.com>
2019-05-14 01:25:59 -0400

    Implemented user login resolver

--- server/src/entity/user/resolvers/UserResolvers.ts
+++ server/src/entity/user/resolvers/UserResolvers.ts
@@ -1,13 +1,27 @@
 import bcrypt from "bcryptjs";
-import { Arg, Mutation, Query, Resolver } from "type-graphql";
+import { Request } from "express";
+import { Arg, Ctx, Mutation, Query, Resolver } from "type-graphql";
 import { User } from "../UserEntity";
 import { UserRegistrationInfo } from "./modules/UserResgistrationInfo";
+
+interface RequestContext {
+  req: Request;
+}
 
 @Resolver()
 export class UserResolvers {
   @Query(() => [User])
   async user() {
     return User.find({ relations: ["sets"] });
+  }
+
+  @Query(() => User, { nullable: true })
+  async me(@Ctx() requestContext: RequestContext) {
+    if (!requestContext.req.session!.userId) {
+      return null;
+    }
+
+    return User.findOne(requestContext.req.session!.userId);
   }
 
   @Mutation(() => User)
@@ -21,6 +35,26 @@ export class UserResolvers {
       password: hashedPassword,
       ...rest
     }).save();
+    return user;
+  }
+
+  @Mutation(() => User, { nullable: true })
+  async loginUser(
+    @Arg("email") email: string,
+    @Arg("password") password: string,
+    @Ctx() requestContext: RequestContext
+  ): Promise<User | null> {
+    const user = await User.findOne({ where: { email: email } });
+    if (!user) {
+      return null;
+    }
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword) {
+      return null;
+    }
+
+    requestContext.req.session!.userId = user.id;
+
     return user;
   }
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,9 @@
 import { ApolloServer } from "apollo-server-express";
+import connectRedis from "connect-redis";
 import cors from "cors";
 import Express from "express";
+import session from "express-session";
+import Redis from "ioredis";
 import "reflect-metadata";
 import { buildSchema } from "type-graphql";
 import { createConnection } from "typeorm";
@@ -21,7 +24,8 @@ const main = async () => {
     emitSchemaFile: process.env.NODE_ENV === "development" ? true : false
   });
   const server = new ApolloServer({
-    schema
+    schema,
+    context: ({ req }: any) => ({ req })
   });
 
   const app = Express();
@@ -36,6 +40,27 @@ const main = async () => {
         process.env.NODE_ENV === "production"
           ? "https://overload-client.herokuapp.com"
           : "http://localhost:3000"
+    })
+  );
+
+  const RedisStore = connectRedis(session);
+  const redis = new Redis();
+
+  app.use(
+    session({
+      store: new RedisStore({
+        client: redis as any
+      }),
+      name: "userStore",
+      // TODO - put sensitive config data in env vars
+      secret: "put me in emv file",
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 1000 * 60 * 60 * 24 * 7 * 365
+      }
     })
   );
 
