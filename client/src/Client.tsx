ae58bfc8c075122ac1fff05184fca3af80798004
purpleshell <purpleshell@github.com>
2019-05-15 20:15:42 -0400

    Implement Login page for client site

--- client/src/Client.tsx
+++ client/src/Client.tsx
@@ -1,7 +1,8 @@
 import ApolloClient from "apollo-boost";
 import React from "react";
 import { ApolloProvider, Query } from "react-apollo";
-import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import LoginUserForm from "./components/LoginUserForm";
 import MobileNav from "./components/MobileNav";
 import { AM_I_LOGGED_IN } from "./components/Schema";
 import ExercisePage from "./pages/ExercisePage";
@@ -25,7 +26,7 @@ const Client = () => {
           {({ loading, error, data }) => {
             // while waiting for query response, render onboarding site
             if (loading) return <Site />;
-            if (error) return <p>{error.message}</p>;
+            if (error) return <Site />;
 
             // if user is not logged in, render the marketing website
             if (data.me) {
@@ -42,83 +43,10 @@ const Client = () => {
 
 const Site = () => {
   return (
-    <div className="site">
-      <div className="site-background-blur">
-        <div className="container">
-          <Router>
-            <header>
-              <div className="logo">
-                <span className="primary">OVER</span>LOAD
-              </div>
-              <nav>
-                <Link className="site-nav-link link" to="/login">
-                  LOGIN
-                </Link>
-              </nav>
-            </header>
-            <main>
-              <h1 className="site-heading">Your Personal Fitness Database</h1>
-              <h2 className="app-details-preview">
-                Record your training. View tailor made logs, graphs, diagrams
-                and suggestions for your individual data.
-              </h2>
-              <h3>Plans starting at $1.99/month.</h3>
-              <button className="cta-btn">START YOUR FREE TRIAL</button>
-              <h4>Free trial available for new subscribers only.</h4>
-              <Link className="learn-more-link link" to="/learn-more">
-                Learn More
-                <div className="icon-wrapper">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                    <path d="M16.172 9l-6.071-6.071 1.414-1.414L20 10l-.707.707-7.778 7.778-1.414-1.414L16.172 11H0V9z" />
-                  </svg>
-                </div>
-              </Link>
-            </main>
-            <footer>
-              <a
-                className="photo-cred"
-                style={{
-                  backgroundColor: "black",
-                  color: "white",
-                  textDecoration: "none",
-                  padding: "4px 6px",
-                  fontSize: "12px",
-                  fontWeight: "bold",
-                  lineHeight: 1.2,
-                  display: "inline-block",
-                  borderRadius: "3px"
-                }}
-                href="https://unsplash.com/@aloragriffiths?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge"
-                target="_blank"
-                rel="noopener noreferrer"
-                title="Download free do whatever you want high-resolution photos from Alora Griffiths"
-              >
-                <span style={{ display: "inline-block", padding: "2px 3px" }}>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    style={{
-                      height: "12px",
-                      width: "auto",
-                      position: "relative",
-                      verticalAlign: "middle",
-                      top: "-2px",
-                      fill: "white"
-                    }}
-                    viewBox="0 0 32 32"
-                  >
-                    <title>unsplash-logo</title>
-                    <path d="M10 9V0h12v9H10zm12 5h10v18H0V14h10v9h12v-9z" />
-                  </svg>
-                </span>
-                <span style={{ display: "inline-block", padding: "2px 3px" }}>
-                  Alora Griffiths
-                </span>
-              </a>
-            </footer>
-          </Router>
-        </div>
-      </div>
-    </div>
+    <Router>
+      <Route path="/" exact component={LandingPage} />
+      <Route path="/login" exact component={LoginUserForm} />
+    </Router>
   );
 };
 
@@ -130,7 +58,7 @@ const App = () => {
           <MobileNav />
         </header>
         <main>
-          <Route path="/" exact component={LandingPage} />
+          <Route path="/" exact component={ProfilePage} />
           <Route exact path="/exercises/" component={ExercisesPage} />
           <Route path="/exercise/:id" component={ExercisePage} />
           <Route exact path="/profile/" component={ProfilePage} />
--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -1,7 +1,84 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const LandingPage = () => {
-  return <h1>Landing Page</h1>;
+  return (
+    <div className="site">
+      <div className="site-background-blur">
+        <div className="container">
+          <header>
+            <div className="logo">
+              <span className="primary">OVER</span>LOAD
+            </div>
+            <nav>
+              <Link className="site-nav-link link" to="/login">
+                LOGIN
+              </Link>
+            </nav>
+          </header>
+          <main>
+            <h1 className="site-heading">Your Personal Fitness Database</h1>
+            <h2 className="app-details-preview">
+              Record your training. View tailor made logs, graphs, diagrams and
+              suggestions for your individual data.
+            </h2>
+            <h3>Plans starting at $1.99/month.</h3>
+            <button className="cta-btn">START YOUR FREE TRIAL</button>
+            <h4>Free trial available for new subscribers only.</h4>
+            <Link className="learn-more-link link" to="/learn-more">
+              Learn More
+              <div className="icon-wrapper">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                  <path d="M16.172 9l-6.071-6.071 1.414-1.414L20 10l-.707.707-7.778 7.778-1.414-1.414L16.172 11H0V9z" />
+                </svg>
+              </div>
+            </Link>
+          </main>
+          <footer>
+            <a
+              className="photo-cred"
+              style={{
+                backgroundColor: "black",
+                color: "white",
+                textDecoration: "none",
+                padding: "4px 6px",
+                fontSize: "12px",
+                fontWeight: "bold",
+                lineHeight: 1.2,
+                display: "inline-block",
+                borderRadius: "3px"
+              }}
+              href="https://unsplash.com/@aloragriffiths?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="Download free do whatever you want high-resolution photos from Alora Griffiths"
+            >
+              <span style={{ display: "inline-block", padding: "2px 3px" }}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  style={{
+                    height: "12px",
+                    width: "auto",
+                    position: "relative",
+                    verticalAlign: "middle",
+                    top: "-2px",
+                    fill: "white"
+                  }}
+                  viewBox="0 0 32 32"
+                >
+                  <title>unsplash-logo</title>
+                  <path d="M10 9V0h12v9H10zm12 5h10v18H0V14h10v9h12v-9z" />
+                </svg>
+              </span>
+              <span style={{ display: "inline-block", padding: "2px 3px" }}>
+                Alora Griffiths
+              </span>
+            </a>
+          </footer>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LandingPage;
