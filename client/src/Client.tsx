706352d1e0aeefa3308bbb789cbf391e4e9618b6
purpleshell <purpleshell@github.com>
2019-05-20 11:19:16 -0400

    adjust comments

--- client/src/Client.tsx
+++ client/src/Client.tsx
@@ -24,15 +24,15 @@ const Client = () => {
       <ApolloProvider client={apolloclient}>
         <Query query={AM_I_LOGGED_IN}>
           {({ loading, error, data }) => {
-            // while waiting for query response, render onboarding site
+            // while waiting for query response, we want to render the onboarding site
             if (loading) return <Site />;
             if (error) return <Site />;
 
-            // if user is not logged in, render the marketing website
+            // if user is logged in, we want to render the app
             if (data.me) {
               return <App />;
             }
-            // if user is logged in, render the app
+            // if user is not logged in, we want to render the onboarding site
             return <Site />;
           }}
         </Query>
