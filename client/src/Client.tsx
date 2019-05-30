6c46246a7acc12307899c082925e3e3217b9354d
purpleshell <purpleshell@github.com>
2019-05-30 16:16:55 -0400

    Attempt to fix typescript apollo query error

--- client/src/Client.tsx
+++ client/src/Client.tsx
@@ -2,7 +2,6 @@ import ApolloClient from "apollo-boost";
 import React from "react";
 import { ApolloProvider, Query } from "react-apollo";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import LoginUserForm from "./components/forms/LoginUserForm";
 import MobileNav from "./components/MobileNav";
 import { AM_I_LOGGED_IN } from "./components/Schema";
 import ExercisePage from "./pages/ExercisePage";
@@ -45,7 +44,6 @@ const Site = () => {
   return (
     <Router>
       <Route path="/" component={LandingPage} />
-      <Route path="/login" exact component={LoginUserForm} />
     </Router>
   );
 };
