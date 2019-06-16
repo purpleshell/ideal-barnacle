0a948c50dfa7365ca81d39818d7d5264d4494bef
purpleshell <purpleshell@github.com>
2019-06-16 12:27:24 -0400

    Refactor TargetMuscle enum to be shared between client and server via common package

---
+++ client/heroku.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Build script for https://github.com/croaky/heroku-buildpack-monorepo
+
+## The proceeding create react app buildback is going to run our build script. This is a placeholder file
+
+echo "Transferring build responsibilities to CRA buildpack."
